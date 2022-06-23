fc58c23a4d1ec9eac1972ee50af79aa7f11d2d9e
FelixMorenoPenarrubia <44442615+FelixMorenoPenarrubia@users.noreply.github.com>
2022-06-23 19:17:24 +0200

    Update sumas.cpp

--- 2022/Final Día 1/sumas.cpp
+++ 2022/Final Día 1/sumas.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <unordered_set>
+#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
@@ -8,17 +8,19 @@ int main() {
 	int n, m;
 	cin >> n >> m;
 
-	unordered_set<int> mod_set;
+	unordered_map<int, int> mod_set;
 
 	int count_0 = 0;
 	int count_1_2 = 0;
 	int count_gen = 1;
+	
+	bool nm1eq = false;
 
 	for (int i = 0; i < n; ++i) {
 		int k;
 		cin >> k;
 		int mod = k%m;
-
+		
 		if (mod == 0) {
 			++count_0;
 		}
@@ -27,10 +29,15 @@ int main() {
 		}
 		else {
 			if (mod_set.count(mod)) count_gen = 2;
-			mod_set.insert(m-mod);
+			mod_set[m-mod]++;
+			if(mod_set[m-mod] == n-1) {
+				nm1eq = true;
+			}
 		}
 	}	
 	int res = max({count_0, count_1_2, count_gen});
-	if (2*res > n) cout << -1 << '\n';
+	if (count_gen == 1 && count_0 <= 1 && count_1_2 <= 1) cout << 1 << '\n';
+	else if (n < 4 || nm1eq) cout << -1 << '\n';
+	else if (2*res > n) cout << -1 << '\n';
 	else cout << res << '\n';
 }
